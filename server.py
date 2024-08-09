9fed22d997499fbace9bc49958a3692dd0333b64
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-09 10:28:46 -0700

    Binded socket to port, allows for socket listening for a connection

--- server.py
+++ server.py
@@ -12,3 +12,25 @@ def socket_create():
         s = socket.socket()
     except socket.error as msg:
         print("Sockt creation error: " + str(msg))
+
+#Bind socket to port and wait for onnection from client 
+def socket_bind():
+    try:
+        global host 
+        global port 
+        global s 
+        print("Binding socket to port: " + str(port))
+        s.bind((host,port))
+        #allows server to accecpt coonection (5 tries before refeusing)
+        s.listen(5)
+    except socket.error as msg:
+        print("Socket binding error: " + str(msg) + "\n" + "Retrying")
+        socket_bind()
+
+
+#Establish a connection with client (socket must be listening for them)
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established | " + " IP " + address[0] + " | Port " + str(address[1]))
+    send_commands(conn)
+    conn.close()
