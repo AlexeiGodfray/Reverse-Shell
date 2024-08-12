1679d8a73a24777b8b1b6853d044f98b5b672564
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-12 11:03:30 -0700

    a simple main function that allows for simple debugging

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ import socket
 import sys
 
 # create socket (allows two computers to conncect)
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
 def socket_create():
     try:
         global host 
@@ -34,3 +40,18 @@ def socket_accept():
     print("Connection has been established | " + " IP " + address[0] + " | Port " + str(address[1]))
     send_commands(conn)
     conn.close()
+
+#send commands
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'exit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response , end="")
+
+main()
